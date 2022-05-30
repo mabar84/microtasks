8283ee8d2772e3e00b8d92e8e6b5e1d732db8dc6
Baranouski <mabar@tut.by>
2022-05-31 02:20:17 +0300

    les2 'map' is done

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,39 @@ import './App.css';
 import {Header} from "./site/Header";
 import {Body} from "./site/Body";
 import {Footer} from "./site/Footer";
+import {NewComponent} from "./site/NewComponent";
 
 
 function App() {
+
+    const students = [
+        {id: 1, name: "James", age: 8},
+        {id: 2, name: "Robert", age: 18},
+        {id: 3, name: "John", age: 28},
+        {id: 4, name: "Michael", age: 38},
+        {id: 5, name: "William", age: 48},
+        {id: 6, name: "David", age: 58},
+        {id: 7, name: "Richard", age: 68},
+        {id: 8, name: "Joseph", age: 78},
+        {id: 9, name: "Thomas", age: 88},
+        {id: 10, name: "Charles", age: 98},
+        {id: 11, name: "Christopher", age: 100},
+    ]
+
+    const topCars = [
+        {manufacturer:"BMW",model:'m5cs'},
+        {manufacturer:"Mercedes",model:'e63s'},
+        {manufacturer:"Audi",model:'rs6'}
+    ]
+
     return (
         <>
-            <div className="App">
-                <Header text={"logo"}/>
-                <Body titleForBody={"content"}/>
-                <Footer titleForFooter={"contacts"}/>
-            </div>
+
+                {/*<Header text={"logo"}/>*/}
+                <NewComponent students={students} topCars={topCars}/>
+                {/*<Body titleForBody={"content"}/>*/}
+                {/*<Footer titleForFooter={"contacts"}/>*/}
+
         </>
     );
 }
