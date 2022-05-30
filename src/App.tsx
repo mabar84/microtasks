d697a979cb9aab61ff35545c415a1a7501edf6e8
Baranouski <mabar@tut.by>
2022-05-31 00:21:25 +0300

    lesson 1 components is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./site/Header";
+import {Body} from "./site/Body";
+import {Footer} from "./site/Footer";
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
-  );
+    return (
+        <>
+            <div className="App">
+                <Header text={"logo"}/>
+                <Body titleForBody={"content"}/>
+                <Footer titleForFooter={"contacts"}/>
+            </div>
+        </>
+    );
 }
 
 export default App;
