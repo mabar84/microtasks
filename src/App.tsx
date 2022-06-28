8e74d5761bf74ccda1878040952e216e03b1e57d
Baranouski <mabar@tut.by>
2022-06-28 14:47:32 +0300

    les4 useState done

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,53 @@
-import React, {useState} from 'react';
-import './App.css';
-import {FullInput} from "./components/FullInput";
-import {Input} from "./components/Input";
-import {Button} from "./components/Button";
+import React, { useState } from "react";
+import "./App.css";
+import { FullInput } from "./components/FullInput";
+import { Input } from "./components/Input";
+import { Button } from "./components/Button";
 
 export type FilterType = "all" | "ruble" | "dollar";
 
 function App() {
+  // const [message, setMessage] = useState([
+  //     {message: "message1"},
+  //     {message: "message2"},
+  //     {message: "message3"},
+  // ])
 
-    const [message, setMessage] = useState([
-        {message: "message1"},
-        {message: "message2"},
-        {message: "message3"},
-    ])
+  // let [title, setTitle] = useState("")
 
-    let [title, setTitle] = useState("")
+  // const addMessage = (title: string) => {
+  //     const newMessages = [{message: title}, ...message]
+  //     setMessage(newMessages)
+  // }
 
-    const addMessage = (title: string) => {
-        const newMessages = [{message: title}, ...message]
-        setMessage(newMessages)
-    }
+  // const callBack = () => {
 
-    const callBack = () => {
+  //     addMessage(title)
+  //     setTitle("")
+  // }
 
-        addMessage(title)
-        setTitle("")
-    }
+  //   let a = 1;
+  let [a, setA] = useState(1);
 
-    return (
-        <div className={"App"}>
+  const onClickHandler = () => {
+    setA(++a);
+    console.log(a);
+  };
 
-            {/*<FullInput addMessage={addMessage}/>*/}
+  const onClickHandler0 = () => {
+    setA(0);
+  };
 
-            <Input title={title}
+  return (
+    <div className={"App"}>
+      <h1>{a}</h1>
+
+      <button onClick={onClickHandler}>number</button>
+      <button onClick={onClickHandler0}>0</button>
+
+      {/*<FullInput addMessage={addMessage}/>*/}
+
+      {/* <Input title={title}
                    setTitle={setTitle}
             />
             <Button name={"+"} callBack={callBack}/>
@@ -41,11 +56,9 @@ function App() {
                 return (
                     <div key={index}>{el.message}</div>
                 )
-            })}
-        </div>
-
-
-    );
+            })} */}
+    </div>
+  );
 }
 
 export default App;
