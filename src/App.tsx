c0f4c34ab195715383a091b13d5975e24d54f67a
mabar84 <mabar@tut.by>
2023-10-10 17:29:07 +0300

    monday mikrotask 4 useState is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button} from "./components/Button";
 
 function App() {
@@ -15,11 +15,26 @@ function App() {
     }
 
 
+    let [a, setA] = useState(1)
+
+    const callBackIncreaseA = () => {
+        setA(++a)
+        console.log(a)
+    }
+    const callBack0 = () => {
+        setA(0)
+        console.log(a)
+    }
+
+
     return (
         <>
+            <h1>{a}</h1>
             <Button callBack={() => callBackFoo1('Vasia')} buttonName={'Channel-1'}/>
             <Button callBack={() => callBackFoo2('Ivan')} buttonName={'Channel-2'}/>
             <Button callBack={callBackFoo3} buttonName={'Stupid'}/>
+            <Button callBack={callBackIncreaseA} buttonName={'a+'}/>
+            <Button callBack={callBack0} buttonName={'0'}/>
         </>
     );
 }
