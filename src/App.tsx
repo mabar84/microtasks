660876d9a0467917db0c4b270937533d6984b4ed
mabar84 <mabar@tut.by>
2023-10-11 10:15:24 +0300

    monday mikrotask 5 useState is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Button} from "./components/Button";
+import {Money} from "./components/money";
 
 function App() {
 
@@ -18,6 +19,8 @@ function App() {
     let [a, setA] = useState(1)
 
     const callBackIncreaseA = () => {
+        // setA((prev) => prev + 1);
+
         setA(++a)
         console.log(a)
     }
@@ -25,6 +28,34 @@ function App() {
         setA(0)
         console.log(a)
     }
+
+    const money = [
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ]
+    //
+    // const [filteredMoney, setFilteredMoney] = useState(money)
+    //
+    // const filterMoney = (filter: string) => {
+    //     setFilteredMoney(money.filter(el => el.banknots === filter))
+    //     if (filter === 'all') {
+    //         setFilteredMoney(money)
+    //     }
+    // }
+    //
+    // const moneyFromMap = filteredMoney.map((el, ind) => {
+    //     return (
+    //         <li key={ind}>
+    //             {el.banknots} {el.value} {el.number}
+    //         </li>
+    //     )
+    // })
 
 
     return (
@@ -35,6 +66,9 @@ function App() {
             <Button callBack={callBackFoo3} buttonName={'Stupid'}/>
             <Button callBack={callBackIncreaseA} buttonName={'a+'}/>
             <Button callBack={callBack0} buttonName={'0'}/>
+
+            <Money money={money}/>
+
         </>
     );
 }
